9d949f7815924298a820e4fbcea2c094c5633ad3
bizu <scbizu@icloud.com>
2016-05-05 15:04:48 +0800

    course
    * 新增查询课表
    * CookieJar 优化
    * Fuck JWC continue......

--- jwclisten/jwcconn.go
+++ jwclisten/jwcconn.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"io"
 	"regexp"
+
 	"io/ioutil"
 	"net/http/cookiejar"
 	 "github.com/scbizu/mahonia"
@@ -18,6 +19,10 @@ var cookies []*http.Cookie
 //唯一标识
 var VIEWSTATE string=""
 const(
+	//用户名
+	username string="studentNo"
+	//密码
+	password string="Yourpassword"
 	//模拟登陆第一个入口地址
 	login_url_gate0 string ="http://210.33.60.8/"
 	//模拟登陆第一个入口验证码地址
@@ -26,24 +31,20 @@ const(
 	logged_url string="http://210.33.60.8/xs_main.aspx?xh=201305070123"
 	//默认登录页
 	default_url string ="http://210.33.60.8/default2.aspx"
-	//用户名
-	username string="YOUR USERNAME"
-	//密码
-	password string="PASSWORD"
-
-
+	//课程表
+	courseURL string="http://210.33.60.8/xskbcx.aspx?xh=201305070123"
 	)
 
 func checkError(err error){
 	if err!=nil{
-		log.Fatal("Connection Fail")
+		log.Fatal(err)
 		}
 	}
 /**
 * 获取这两个不知道干什么的值
 */
-func getsp()map[string]string{
-        view,err:=http.Get(login_url_gate0)
+func getsp(url string)map[string]string{
+        view,err:=http.Get(url)
         checkError(err)
         //去拿__VIEWSTATE
         body,err:=ioutil.ReadAll(view.Body)
@@ -61,10 +62,34 @@ func getsp()map[string]string{
 		return res	
 	}
 /**
+*带cookie去拿第二波不明变量   否则 会出现Object Move <a href>here</a>
+*/
+func getspAG(cookies []*http.Cookie,c *http.Client,name string,userno string) map[string]string{
+		req,_:=http.NewRequest("GET","http://210.33.60.8/xskbcx.aspx?xh="+userno+"&xm="+url.QueryEscape(name)+"&gnmkdm=N121603",nil)
+		for _,v:=range cookies{
+			req.AddCookie(v)
+			}
+		response,_:=c.Do(req)
+        //去拿__VIEWSTATE
+        body,err:=ioutil.ReadAll(response.Body)
+        checkError(err)
+        regular:=`<input.type="hidden".name="__VIEWSTATE".value="(.*)" />`
+        pattern:=regexp.MustCompile(regular)        
+        VIEWSTATE:=pattern.FindAllStringSubmatch(string(body),-1)
+    //拿__VIEWSTATEGENERATOR
+    	retor:=`<input.type="hidden".name="__VIEWSTATEGENERATOR".value="(.*)" />`
+		patterntor:=regexp.MustCompile(retor)
+		VIEWSTATEGENERATOR:=patterntor.FindAllStringSubmatch(string(body),-1)
+		res:=make(map[string]string)
+		res["VIEWSTATE"]=VIEWSTATE[0][1]
+		res["VIEWSTATEGENERATOR"]=VIEWSTATEGENERATOR[0][1]
+		return res			
+	}
+
+/**
 *模拟post表单
 */
-func post(c *http.Client,username string,password string,verify_code string,VIEWSTATE string,VIEWSTATEGENERATOR string,temp_cookies []*http.Cookie) []*http.Cookie{
-	
+func post(Rurl string,c *http.Client,username string,password string,verify_code string,VIEWSTATE string,VIEWSTATEGENERATOR string,temp_cookies []*http.Cookie) []*http.Cookie{
     postValue:=url.Values{}
   	cd:=mahonia.NewEncoder("gb2312")
   	rb:=cd.ConvertString("学生")
@@ -80,40 +105,68 @@ func post(c *http.Client,username string,password string,verify_code string,VIEW
  	postValue.Add("hidsc","")
  	postValue.Add("RadioButtonList1",rb)
  	//开始POST   这次POST到登陆界面   带上第一次请求的cookie 和 验证码  和 一些必要的数据
- 	postUrl,_:=url.Parse(default_url)
+ 	postUrl,_:=url.Parse(Rurl)
  	Jar,_:=cookiejar.New(nil)
  	Jar.SetCookies(postUrl,temp_cookies)
- 	c.Jar=Jar
- 	result,_:=c.PostForm(default_url,postValue)
- 	cookies=result.Cookies()	
+ 	c.Jar=Jar	
+	resp,_:=c.PostForm(Rurl,postValue) 
+	cookies:=resp.Cookies()
  	return cookies
 	}
 /*
 *测试结果
 */
-func Testpage(cookies []*http.Cookie,c *http.Client) string{
- 	//拿到这个登录成功的cookie后  再带着这个cookie 再伪造一次请求去我们想要的URL
- 	
+func Testpage(c *http.Client) string{
+ 	//拿到这个登录成功的cookie后  再带着这个cookie 再伪造一次请求去我们想要的URL	
  	req,err:=http.NewRequest("GET",logged_url,nil)
  	checkError(err)
- 	//traversal last cookies
- 	for _,fv:=range cookies{
- 		req.AddCookie(fv)
- 		fmt.Println(fv)
+ 	for _,v:=range cookies{
+ 	 	req.AddCookie(v)	
  		}
     finalRes,err:=c.Do(req)
  	checkError(err)
  	allData,err:=ioutil.ReadAll(finalRes.Body)
  	checkError(err)
- 	defer finalRes.Body.Close()
+ 	finalRes.Body.Close()
  	return string(allData)	
 	}
 
+//获取学生姓名
+func getStuName(c *http.Client) string{
+ 	req,err:=http.NewRequest("GET",logged_url,nil)
+ 	checkError(err)
+    finalRes,err:=c.Do(req)	
+    checkError(err)
+ 	allData,err:=ioutil.ReadAll(finalRes.Body)
+ 	checkError(err)
+ 	defer finalRes.Body.Close()   
+ 	cd:=mahonia.NewEncoder("gb2312")
+  	rb:=cd.ConvertString("<span.id=\"xhxm\">(.*)同学</span>")
+ 	//Regexp
+ 	regular:=rb
+ 	pattern:=regexp.MustCompile(regular)        
+    stuName:=pattern.FindAllStringSubmatch(string(allData),-1)
+    return stuName[0][1]
+	}
+
+//Get Course info.
+func getCourseData(c *http.Client)string{
+ 	req,err:=http.NewRequest("GET",courseURL,nil)
+ 	//NICE
+ 	req.Header.Set("Referer",courseURL)
+ 	checkError(err)
+    finalRes,err:=c.Do(req)
+ 	checkError(err)
+ 	allData,err:=ioutil.ReadAll(finalRes.Body)
+ 	checkError(err)
+ 	finalRes.Body.Close()
+ 	return string(allData)	
+	}
 
 //MAIN
 
 func main() {
-	viewRes:=getsp()	
+	viewRes:=getsp(login_url_gate0)	
 	VIEWSTATE:=viewRes["VIEWSTATE"]
 	VIEWSTATEGENERATOR:=viewRes["VIEWSTATEGENERATOR"]
 	
@@ -126,7 +179,6 @@ func main() {
    	req.URL, _ = url.Parse(vrcode_url_gate0)
     for _, v := range res.Cookies() {
         req.AddCookie(v)
-        fmt.Println(v)
     }
     // 获取验证码
     var verify_code string
@@ -145,9 +197,20 @@ func main() {
        res.Body.Close()
 		}
     
-	cookies:=post(c,username,password,verify_code,VIEWSTATE,VIEWSTATEGENERATOR,temp_cookies)
- 	
- 	data:=Testpage(cookies,c)
- 	
-	fmt.Println(data)
+ 	cookies:=post(default_url,c,username,password,verify_code,VIEWSTATE,VIEWSTATEGENERATOR,temp_cookies)   
+	fmt.Println(temp_cookies)
+	fmt.Println(cookies)
+//	fmt.Printf("%v\n",c.Jar)
+//	for val,_:=range c.Jar.Cookies{
+//		fmt.Println(val)
+//		}
+ 	data:=Testpage(c)
+ 	fmt.Println(data)	
+ 	Name:=getStuName(c)
+ 	fmt.Println(Name)
+
+ 	course:=getCourseData(c)
+	fmt.Println(course)
 }
+
+
